192c2e4e48535119fb81caae4dde42f893cd1b45
duduool <2859894806@qq.com>
2014-12-29 22:21:11 +0800

    2014-12-29 初始版本，显示地图

---
+++ src/com/wtu/map/BasicMapActivity.java
@@ -0,0 +1,65 @@
+package com.wtu.map;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.amap.api.maps2d.AMap;
+import com.amap.api.maps2d.AMap.OnCameraChangeListener;
+import com.amap.api.maps2d.MapView;
+import com.amap.api.maps2d.model.CameraPosition;
+import com.wtu.map.R;
+ 
+public class BasicMapActivity extends Activity {
+	private MapView mapView;
+	private AMap aMap;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.basicmap_activity);
+		mapView = (MapView) findViewById(R.id.map);
+		mapView.onCreate(savedInstanceState);
+		
+		init();
+	}
+
+	private void init() {
+		if (aMap == null) {
+			aMap = mapView.getMap();
+			aMap.setOnCameraChangeListener(new OnCameraChangeListener() {
+				
+				@Override
+				public void onCameraChangeFinish(CameraPosition cameraPosition) {
+					System.out.println("zoom level is:"+cameraPosition.tilt );
+				}
+				
+				@Override
+				public void onCameraChange(CameraPosition arg0) {
+				}
+			});
+		}
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		mapView.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		mapView.onPause();
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		mapView.onSaveInstanceState(outState);
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		mapView.onDestroy();
+	}
+}
